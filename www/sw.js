18e0a7791ee54f170eef010b9db58ae54e7a1417
Nywayz <48278317+Nywayz@users.noreply.github.com>
2021-11-23 21:51:24 +0100

    d

--- www/sw.js
+++ www/sw.js
@@ -1,14 +1,16 @@
 /** VARS */
 const ORIGIN_URL = `${location.protocol}//${location.host}`;
-const CACHE_NAME = "on est pas en ligne";
+const CACHE_NAME = "offline-v2";
 const OFFLINE_URL = "offline.html";
 const CACHED_FILES = [
   OFFLINE_URL,
   "https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js",
-  `${ORIGIN_URL}/css/index.css`,
   `${ORIGIN_URL}/js/index.js`,
+  `${ORIGIN_URL}/js/pseudoRouteur.js`,
+  `${ORIGIN_URL}/js/top.js`,
   `${ORIGIN_URL}/img/logo.png`,
+  `${ORIGIN_URL}/img/logo144.png`,
 ];
 /** FUNCTIONS */
 
